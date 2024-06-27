bbb4e767d0d1665653908427a82e0d7f7cd67445
TiagoAderbal <108840341+TiagoAderbal@users.noreply.github.com>
2024-06-26 23:09:39 -0300

    first commit

---
+++ pages/books_review.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.set_page_config(layout="wide")
+
+df_reviews = pd.read_csv("pages/customer reviews.csv")
+df_top100 = pd.read_csv("pages/Top-100 Trending Books.csv")
+
+books = df_top100["book title"].unique()
+book = st.sidebar.selectbox("Books", books)
+
+df_book = df_top100[df_top100["book title"] == book]
+df_reviews_f = df_reviews[df_reviews["book name"] == book]
+
+book_title = df_book["book title"].iloc[0]
+book_genre = df_book["genre"].iloc[0]
+book_price = f"${df_book['book price'].iloc[0]}"
+book_rating = df_book["rating"].iloc[0]
+book_year = df_book["year of publication"].iloc[0]
+
+st.title(book_title)
+st.subheader(book_genre)
+col1, col2, col3 = st.columns(3)
+col1.metric("Price", book_price)
+col2.metric("Rating", book_rating)
+col3.metric("Year of Publication", book_year)
+st.divider()
+
+for row in df_reviews_f.values:
+    message = st.chat_message(f"{row[4]}")
+    message.write(f"**{row[2]}**")
+    message.write(row[5])
